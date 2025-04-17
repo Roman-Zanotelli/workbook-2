18e9d66b237f122ab30ff13b2bd2d7391bfe0880
Roman-Zanotelli <Roman-Zanotelli@github.com>
2025-04-17 11:38:16 -0400

    Completed LoopingExercises

---
+++ LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int it = 0;
+        do{
+            System.out.println("I love Java");
+            it++;
+        }while (it < 5);
+    }
+}
---
+++ LoopingExercises/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int it = 0;
+        while (it < 5){
+            System.out.println("I love Java");
+            it++;
+        }
+    }
+}
